ac776a594eca8886e177ddec1490abb116cec4c9
grumbletumbles <dimanaer123@gmail.com>
2024-05-11 22:38:28 +0300

    lab3 task 1 fix

--- lab-3/task-1.sh
+++ lab-3/task-1.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-dates=$(date "+%F|%F")
+dates=$(date "+%F|%T")
 
 mkdir ~/test && { echo "catalog was successfully created" > ~/report ; touch ~/test/$dates ; }
 ping net_nikogo.ru || echo "${dates} ERROR CONNECTING TO HOST" >> ~/report
