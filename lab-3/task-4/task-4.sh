1f9f97638d3e014ee63f91f1c2e6271071a45f0f
grumbletumbles <dimanaer123@gmail.com>
2024-05-17 14:49:08 +0300

    lab3 task 4 fix

--- lab-3/task-4/task-4.sh
+++ lab-3/task-4/task-4.sh
@@ -4,4 +4,17 @@ sh loop.sh&pid0=$!
 sh loop.sh&pid1=$!
 sh loop.sh&pid1=$!
 
-renice +10 -p $pid0
+while true;
+do
+	cpu=$(ps -p $pid0 -o $cpu | tail -n +2)
+	if [[ $cpu -gt 10 ]];
+	do
+		renice +1 -p $pid0
+	done
+
+	if [[ $cpu -lt 9 ]];
+	do
+		renice -1 -p $pid0
+	done
+done
+#renice +10 -p $pid0
