d911d428442e1c424656cde55358c8653642f3fa
grumbletumbles <dimanaer123@gmail.com>
2024-05-17 15:13:37 +0300

    lab3 task 5 fix

--- lab-3/task-4/task-4.sh
+++ lab-3/task-4/task-4.sh
@@ -8,14 +8,14 @@ while true;
 do
 	cpu=$(ps -p $pid0 -o %cpu | tail -n +2)
 	if [[ $(echo "$cpu > 10" | bc -l) ]];
-	do
+	then
 		renice 1 -p $pid0
-	done
+	fi
 
 	if [[ $(echo "$cpu < 9" | bc -l) ]];
-	do
+	then
 		renice -1 -p $pid0
-	done
+	fi
 	sleep .1
 done
 #renice +10 -p $pid0
