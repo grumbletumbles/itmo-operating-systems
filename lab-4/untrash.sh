e1e209e8d1229618c3aacb72cd51b0796c58d514
grumbletumbles <dimanaer123@gmail.com>
2024-05-17 11:15:37 +0300

    lab4 untrash

---
+++ lab-4/untrash.sh
@@ -0,0 +1,117 @@
+#!/bin/bash
+
+trashdir="/home/user/.trash"
+trashlog="/home/user/.trash.log"
+home="/home/user"
+
+if [[ $# > 1 ]];
+then
+	echo "too many arguments"
+	exit 1
+fi
+
+if [[ $# < 1 ]];
+then
+	echo "too few arguments"
+	exit 1
+fi
+
+if [[ ! -d $trashdir ]];
+then
+	echo "trash directory not found"
+	exit 1
+fi
+
+if [[ ! -f $trashlog ]];
+then
+	echo "trash log file not found"
+	exit 1
+fi
+
+if [[ $1 == "" ]];
+then
+	echo "bad input"
+	exit 1
+fi
+
+if [[ $1 == "." ]] || [[ $1 == ".." ]];
+then
+	echo "bad input"
+	exit 1
+fi
+
+if [[ ! $key =~ ^[0-9a-zA-Z._-]+$ ]];
+then
+	echo "bad input"
+	exit 1
+fi
+
+
+_first=$(echo $1 | cut -c1-1)
+if ! [[ $first ~= ^[0-9a-zA-Z.]+$ ]];
+then
+	echo "bad input"
+	exit 1
+fi
+
+
+f=$1
+a=""
+# TODO change the lab dir to the correct one
+for i in $(grep "$1" $trashlog | awk '{ print $1 }')
+do
+	a=$(echo $i | sed -e 's,/home/user/lab4/,,')
+	if [ $a == $f ];
+	then 
+		break
+	fi
+done
+
+if [[ $a != $f ]];
+then
+	echo "file not found in trash log"
+	exit 1
+fi
+
+for i in $(grep "$1" $trashlog | awk '{ print $NF }')
+do
+	file=$(grep $i $trashlog | awk '{ $NF=""; print $0 }')
+	file=$(echo "$filename" | sed 's/ *$//')
+	read -p "${filename} Are you sure?: [y/n] " ans
+	case "$ans" in
+		"y")
+			newfilename=""
+			num=$i
+			restoredir=$(echo "$filename" | awk 'BEGIN{FS=OFS="/"}; { $NF=""; print $0 }')
+			filename=$(echo "$file" | awk 'BEGIN{FS="/"}; { print $NF }')
+			if [[ ! -d $restoredir ]];
+			then
+				echo "Directory ${restoredir} not found. File \"${filename}\" will be restored in home directory."
+				if [[ -f "${home}/${filename}" ]];
+				then
+					read -p "File \"${filename}\ already exists. Enter the new name: " newfilename
+					ln "${trashdir}/${num}" "${home}/${newfilename}"
+					rm "${trashdir}/${num}" 
+				else
+					ln "${trashdir}/${num}" "${home}/${filename}"
+					rm "${trashdir}/${num}"
+				fi
+			else
+				if [[ -f "${filename}"]];
+				then
+					read -p "File \"${filename}\ already exists. Enter the new name: " newfilename
+					ln "${trashdir}/${num}" "${restoredir}/${newfilename}"
+					rm "${trashdir}/${num}" 
+				else
+					ln "${trashdir}/${num}" "${newfilename}"
+					rm "${trashdir}/${num}" 
+				fi
+			fi
+			sed "/${num}/d" $trashlog > .trash.log2 && mv .trash.log2 $trashlog
+			echo "success"
+			;;
+		*)
+			continue
+			;;
+	esac
+done
