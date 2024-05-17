e2e4e86381643f53bfa05a5cd5fc32ebe4a56813
grumbletumbles <dimanaer123@gmail.com>
2024-05-17 15:26:04 +0300

    lab3 task 5 fix

--- lab-3/task-4/task-4.sh
+++ lab-3/task-4/task-4.sh
@@ -4,18 +4,21 @@ sh loop.sh&pid0=$!
 sh loop.sh&pid1=$!
 sh loop.sh&pid1=$!
 
+nice=0
+
 while true;
 do
 	cpu=$(ps -p $pid0 -o %cpu | tail -n +2)
 	if [[ $(echo "$cpu > 10" | bc -l) ]];
 	then
-		renice 1 -p $pid0
+		nice=$(($nice + 1))
 	fi
 
 	if [[ $(echo "$cpu < 9" | bc -l) ]];
 	then
-		renice -1 -p $pid0
+		nice=$(($nice - 1))
 	fi
+	renice $nice -p $pid0
 	sleep .1
 done
 #renice +10 -p $pid0
