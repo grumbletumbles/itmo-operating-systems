6255a1cdb9d38e44f67413db2617137529f64712
grumbletumbles <dimanaer123@gmail.com>
2024-05-31 12:37:57 +0300

    lab5 stuff

---
+++ lab-5/mem.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+declare -a array
+declare -a numbers=(1,2,3,4,5,6,7,8,9,10)
+
+rm report.log
+while true;
+do
+	array+=(${numbers[@]})
+	let counter++
+	if [[ $counter == 10000 ]];
+	then
+		echo "${#array[@]}" >> report.log
+	fi
+done
